cfee8fd7930171beaa6f8e94e9bfa3d9f0a2623a
nalekseev <alekseevn2306@gmail.com>
2026-04-12 15:56:24 +0400

    изменил шаблоны профилей, настроил валидацию для форм пользователей

--- messageboard/users/forms.py
+++ messageboard/users/forms.py
@@ -6,50 +6,86 @@ from django.core.exceptions import ValidationError
 
 User = get_user_model()
 
-class CustomUserCreationForm(UserCreationForm):
-    """Форма регистрации пользователя"""
+phone_regex = RegexValidator(
+    regex=r'^\+7\d{10}$',
+    message="Введите номер телефона в формате: +7XXXXXXXXXX"
+)
+
+
+class BaseUserForm(forms.ModelForm):
+    """Базовый класс с общими полями и валидацией"""
     
-    phone_regex = RegexValidator(
-        regex=r'^\+7\d{10}$',
-        message="Введите номер телефона в формате: +7XXXXXXXXXX"
+    username = forms.CharField(
+        label='Имя пользователя',
+        widget=forms.TextInput(attrs={'class': 'form-control'})
     )
-    
+
     phone = forms.CharField(
         label='Телефон',
         required=True,
         validators=[phone_regex],
-        widget=forms.TextInput(attrs={
-            'class': 'form-control',
-            'placeholder': '+7XXXXXXXXXX'
-        })
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '+7XXXXXXXXXX'})
     )
     
     city = forms.CharField(
         label='Город',
         required=False,
-        widget=forms.TextInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Москва'
-        })
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Москва'})
     )
     
     email = forms.EmailField(
         label='Email',
         required=True,
-        widget=forms.EmailInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'email@example.com'
-        })
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'email@example.com'})
     )
+    
+    first_name = forms.CharField(
+        label='Имя',
+        required=False,
+        min_length=2,
+        max_length=20,
+        error_messages={
+            'min_length': 'Имя должно содержать минимум 2 символа',
+            'max_length': 'Имя не может быть длиннее 20 символов'
+        },
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Иван'})
+    )
+    
+    last_name = forms.CharField(
+        label='Фамилия',
+        required=False,
+        min_length=2,
+        max_length=20,
+        error_messages={
+            'min_length': 'Фамилия должна содержать минимум 2 символа',
+            'max_length': 'Фамилия не может быть длиннее 20 символов'
+        },
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Иванов'})
+    )
+    
+    @staticmethod
+    def validate_name(value, field_name):
+        """Общая валидация имени/фамилии"""
+        if value and any(char.isdigit() for char in value):
+            raise ValidationError(f'{field_name} не может содержать цифры')
+        return value
+    
+    def clean_first_name(self):
+        return self.validate_name(self.cleaned_data.get('first_name'), 'Имя')
+    
+    def clean_last_name(self):
+        return self.validate_name(self.cleaned_data.get('last_name'), 'Фамилия')
+    
+    class Meta:
+        abstract = True
+
+
+class CustomUserCreationForm(UserCreationForm, BaseUserForm):
+    """Форма регистрации пользователя"""
     
     class Meta(UserCreationForm.Meta):
         model = User
         fields = ('username', 'email', 'phone', 'city', 'first_name', 'last_name')
-        widgets = {
-            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Имя пользователя'}),
-            'first_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Иван'}),
-            'last_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Иванов'}),
-        }
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -63,49 +99,8 @@ class CustomUserCreationForm(UserCreationForm):
         return user
 
 
-class CustomUserChangeForm(UserChangeForm):
+class CustomUserChangeForm(BaseUserForm, UserChangeForm):
     """Форма редактирования профиля с проверкой уникальности username"""
-    
-    phone_regex = RegexValidator(
-        regex=r'^\+7\d{10}$',
-        message="Введите номер телефона в формате: +7XXXXXXXXXX"
-    )
-    
-    username = forms.CharField(
-        label='Имя пользователя',
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    
-    email = forms.EmailField(
-        label='Email',
-        required=True,
-        widget=forms.EmailInput(attrs={'class': 'form-control'})
-    )
-    
-    phone = forms.CharField(
-        label='Телефон',
-        required=False,
-        validators=[phone_regex],
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    
-    city = forms.CharField(
-        label='Город',
-        required=False,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    
-    first_name = forms.CharField(
-        label='Имя',
-        required=False,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    
-    last_name = forms.CharField(
-        label='Фамилия',
-        required=False,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
     
     class Meta:
         model = User
@@ -114,14 +109,11 @@ class CustomUserChangeForm(UserChangeForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['password'].widget = forms.HiddenInput()
-        # self.fields['username'].help_text = None
+        self.fields['phone'].required = False
     
     def clean_username(self):
         """Проверка уникальности username"""
         username = self.cleaned_data.get('username')
-        if username:
-            # Проверяем, существует ли пользователь с таким username
-            if User.objects.filter(username=username).exclude(pk=self.instance.pk).exists():
-                raise ValidationError('Пользователь с таким именем уже существует.')
+        if username and User.objects.filter(username=username).exclude(pk=self.instance.pk).exists():
+            raise ValidationError('Пользователь с таким именем уже существует.')
         return username
-    
